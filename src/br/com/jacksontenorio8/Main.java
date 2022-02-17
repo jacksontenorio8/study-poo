d3f1dbebd4d09b715f0c3d87b20df6cba234c301
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-02-17 14:27:54 -0300

    Tercerio commit onde foi adiconado ao porjeto os conceitos de abstração e encapsulamento.

--- src/br/com/jacksontenorio8/Main.java
+++ src/br/com/jacksontenorio8/Main.java
@@ -1,8 +1,30 @@
 package br.com.jacksontenorio8;
 
+import br.com.jacksontenorio8.dominio.Curso;
+import br.com.jacksontenorio8.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso de java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso de js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria de java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
